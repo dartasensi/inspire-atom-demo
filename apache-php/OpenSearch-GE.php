3554d7a1799bff6543d31d7a6db5ce0a70302c57
dartasensi <dartasensi@users.noreply.github.com>
2021-11-17 19:19:51 +0100

    Added some sample data, with reference and license about its source

---
+++ apache-php/OpenSearch-GE.php
@@ -0,0 +1,52 @@
+<?php 
+$returnFile = false;
+
+$topFeedURL = "https://sgi3.isprambiente.it/download/GE.atom.TOP.en.xml";
+$datasetFeedURL = "https://sgi3.isprambiente.it/download/GE.atom.en.xml";
+
+foreach (apache_request_headers() as $name => $value) {
+	if ($name=="Accept" && $value=="application/atom+xml") {
+		$returnFile = true;
+	}
+}
+// parse URL parameters
+$q= $_GET['q'];
+$uriCode = $_GET['spatial_dataset_identifier_code'];
+$uriNamespace= $_GET['spatial_dataset_identifier_namespace'];
+$crs= $_GET['crs'];
+$language= $_GET['language'];
+
+if (!$uriCode) {
+	if (!$q) {
+		header("Location: " + $topFeedURL);
+		exit;
+	}
+	$uriCode = $q;
+}
+
+if (!$language || $language == "*"){
+	// default language
+	$language = "en";
+}
+if ($language != 'en' && $language != 'it'){
+	die( "Only en and it languages are supported" );
+}
+
+if (!$uriNamespace || $uriNamespace == ""){
+	// default namespace
+	$uriNamespace = "http://sgi.isprambiente.it";
+}
+
+if ($uriCode == "ispra_rm:Carta_Geologica_Armonizzata_1:100k_DT" &&
+	$uriNamespace == "http://sgi.isprambiente.it" &&
+	$crs == "http://www.opengis.net/def/crs/EPSG/0/4258") {
+	if ($returnFile){
+		header("Location: " + datasetFeedURL);
+	} else{
+		header("Location: " + datasetFeedURL);
+	}
+	exit;
+}
+	
+echo 'Not found';
+?>
---
+++ apache-php/README.md
@@ -0,0 +1,4 @@
+Data source:
+https://catalogosgi.isprambiente.it/geoportalAdm2/catalog/search/resource/details.page?uuid=ispra_rm%3AGeo100k_DT
+
+under license CC-BY-4.0 - https://creativecommons.org/licenses/by/4.0/deed.it
